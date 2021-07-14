16d1eaccddfcff78f39ddfc6db39063f84a24e62
M-Wagdy <mohamed.h.wagdy@gmail.com>
2021-07-14 14:06:07 +0200

    project init

---
+++ SPI_Driver/SPI_Driver/main.c
@@ -0,0 +1,18 @@
+/*
+ * SPI_Driver.c
+ *
+ * Created: 7/14/2021 2:05:16 PM
+ * Author : Dr. Sahar
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
