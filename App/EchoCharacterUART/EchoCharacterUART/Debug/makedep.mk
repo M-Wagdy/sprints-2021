b1e32f562fe0cb3fc07672c104ef9ac3756d36f9
M-Wagdy <mohamed.h.wagdy@gmail.com>
2021-08-01 19:18:15 +0200

    Update folder structure

---
+++ App/EchoCharacterUART/EchoCharacterUART/Debug/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+..\..\..\Mcal\Atmega32\UART\UART.c
+
+main.c
+
+Mcal\UART\UART_Config.c
+
---
+++ Unit Tests/Mcal/Atmega32/UART/UART/MockUARTRegisters.c
@@ -0,0 +1,15 @@
+/*
+ * MockUARTRegisters.c
+ *
+ * Created: 7/13/2021 6:05:33 PM
+ *  Author: Mohamed Wagdy
+ */ 
+#include "../../../../../Infrastructure/STD_types.h"
+
+/* initialize registers valaues */
+uint8_t UART_DATA_R = 0x00;
+uint8_t UART_STATUS_R = 0x40;
+uint8_t UART_CONTROL_R = 0x00;
+uint8_t UART_CONTROL_2_R = 0x86;
+uint8_t UART_BAUDRATE_LOW_R = 0x00;
+uint8_t UART_BAUDRATE_HIGH_R = 0x00;
