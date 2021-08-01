77be1b6d756a7d82cec0cf2f2cb7e0521e384c47
M-Wagdy <mohamed.h.wagdy@gmail.com>
2021-08-01 18:16:59 +0200

    Update folder structure

---
+++ App/BlinkLED/BlinkLED/Mcal/Timer/Timer_PbCfg.c
@@ -0,0 +1,16 @@
+/*
+ * Timer_PbCfg.c
+ *
+ * Created: 7/8/2021 1:33:54 PM
+ *  Author: Mohamed Wagdy
+ */ 
+
+/*- INCLUDES
+----------------------------------------------*/
+#include "Mcal/Atmega32/Timer/Timer_Pbcfg.h"
+
+/* struct containing all DIO pins configurations. */
+const STR_TIMER_config_t gastr_TIMER_configurations[TIMERS_NUMBER] = {
+   {TIMER_0, NORMAL_MODE, NO_INTERRUPT},
+   {TIMER_2, NORMAL_MODE, NO_INTERRUPT}
+};
