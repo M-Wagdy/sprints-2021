b9f614815ff498ca7d70bb28b4cc7766d5036039
M-Wagdy <mohamed.h.wagdy@gmail.com>
2021-08-02 19:43:44 +0200

    Update folder structure

---
+++ App/EchoTerminalStringSPI/EchoTerminalStringSPI/Debug/Mcal/Atmega32/UART/UART.d
@@ -0,0 +1,20 @@
+Mcal/Atmega32/UART/UART.d Mcal/Atmega32/UART/UART.o: \
+ ../../../../Mcal/Atmega32/UART/UART.c \
+ ../../../../Mcal/Atmega32/UART/UART.h \
+ h:\mohamed\ sprints\sprints-2021\infrastructure\std_types.h \
+ ../../../../Mcal/Atmega32/UART/UART_Registers.h \
+ ../../../../Mcal/Atmega32/UART/UART_Config.h \
+ ../../../../Infrastructure/STD_types.h \
+ ../../../../Mcal/Atmega32/UART/UART_retval.h
+
+../../../../Mcal/Atmega32/UART/UART.h:
+
+h:\mohamed\ sprints\sprints-2021\infrastructure\std_types.h:
+
+../../../../Mcal/Atmega32/UART/UART_Registers.h:
+
+../../../../Mcal/Atmega32/UART/UART_Config.h:
+
+../../../../Infrastructure/STD_types.h:
+
+../../../../Mcal/Atmega32/UART/UART_retval.h:
