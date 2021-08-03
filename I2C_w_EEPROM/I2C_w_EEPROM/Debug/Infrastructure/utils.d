ecb0bb0c4712755adcb62d428c03b11d5615ee7e
M-Wagdy <mohamed.h.wagdy@gmail.com>
2021-08-03 11:36:08 +0200

    Update Folder structure

--- I2C_w_EEPROM/I2C_w_EEPROM/Debug/Infrastructure/utils.d
+++
@@ -1,6 +0,0 @@
-Infrastructure/utils.d Infrastructure/utils.o: ../Infrastructure/utils.c \
- ../Infrastructure/utils.h ../Infrastructure/STD_types.h
-
-../Infrastructure/utils.h:
-
-../Infrastructure/STD_types.h:
