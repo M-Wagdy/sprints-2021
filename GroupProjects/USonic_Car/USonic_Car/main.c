9a8aa072c9dcb2694c153f3940b95478f96f7cfa
M-Wagdy <mohamed.h.wagdy@gmail.com>
2021-08-09 11:57:00 +0200

    Init Project

---
+++ GroupProjects/USonic_Car/USonic_Car/main.c
@@ -0,0 +1,18 @@
+/*
+ * USonic_Car.c
+ *
+ * Created: 8/9/2021 11:56:36 AM
+ * Author : Dr. Sahar
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
