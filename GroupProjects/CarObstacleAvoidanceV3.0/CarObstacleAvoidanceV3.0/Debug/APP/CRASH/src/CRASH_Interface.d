3ed610de38528a7c824f1b2c2144cf336da4291a
M-Wagdy <mohamed.h.wagdy@gmail.com>
2021-09-02 16:48:23 +0200

    Added Group Code

--- GroupProjects/CarObstacleAvoidanceV3.0/CarObstacleAvoidanceV3.0/Debug/APP/CRASH/src/CRASH_Interface.d
+++
@@ -1,7 +0,0 @@
-APP/CRASH/src/CRASH_Interface.d APP/CRASH/src/CRASH_Interface.o: \
- ../APP/CRASH/src/CRASH_Interface.c ../APP/CRASH/inc/CRASH_Interface.h \
- ../LIB/STD_TYPES.h
-
-../APP/CRASH/inc/CRASH_Interface.h:
-
-../LIB/STD_TYPES.h:
