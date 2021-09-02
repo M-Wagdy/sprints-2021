bc19a2c5ae4e273aa08d6c78f8bb3f3423614807
M-Wagdy <mohamed.h.wagdy@gmail.com>
2021-09-02 16:37:26 +0200

    V2 Latest Updates

--- GroupProjects/CarObstacleAvoidanceV2.0/V2.0/V2.0/Debug/APP/SENSING/scr/SENSING_Interface.d
+++
@@ -1,7 +0,0 @@
-APP/SENSING/scr/SENSING_Interface.d APP/SENSING/scr/SENSING_Interface.o: \
- ../APP/SENSING/scr/SENSING_Interface.c \
- ../APP/SENSING/inc/SENSING_Interface.h ../LIB/STD_TYPES.h
-
-../APP/SENSING/inc/SENSING_Interface.h:
-
-../LIB/STD_TYPES.h:
